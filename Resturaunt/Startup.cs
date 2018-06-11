d4169f4dd507ad95eaac8824e2e5c829df752085
Visdroid <aldrinvisser@gmail.com>
2018-06-11 19:52:40 +0200

    Add project files.

---
+++ Resturaunt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Resturaunt.Startup))]
+namespace Resturaunt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
